e222966da1c15e7db45f86d4b9c0b6873b7b57e9
jasonrhalpern <jasonrhalpern@gmail.com>
2015-03-12 09:38:22 -0400

    adding avatar upload to devise views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    registration_params = [:first_name, :last_name, :email, :password, :password_confirmation]
+    registration_params = [:first_name, :last_name, :email, :avatar, :password, :password_confirmation]
 
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(registration_params) }
     devise_parameter_sanitizer.for(:account_update) { |u| u.permit(registration_params << :current_password) }
